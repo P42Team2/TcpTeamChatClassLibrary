8baffded490a25b41d5217615f0c0df408ddd469
serhii.ruban81 <serhii.ruban81@gmail.com>
2026-06-04 20:32:12 +0300

    Add user class

---
+++ TcpTeamChatClassLibrary/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpTeamChatClassLibrary.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; } = default!;
+
+        public string Password { get; set; } = default!;
+
+        public string IpAddressStr { get; set; } = "127.0.0.1";
+
+        public int Port { get; set; }
+
+        public UserStatus Status { get; set; }
+    }
+}
